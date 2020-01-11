1b97af7788f07ae25e10e7231e24614dcdbd5489
Nathaniel Harper <harp0129@algonquinlive.com>
2020-01-10 21:52:13 -0500

    Updated js file

--- script.js
+++ script.js
@@ -5,9 +5,3 @@ console.log(`Hello my name is ${name}`)
 const number = 4
 
 console.log((((number * 2) * 5) / number) - 7)
-
-function help () {
-  return `Hello my name is ${name}`
-}
-
-console.log(help())
